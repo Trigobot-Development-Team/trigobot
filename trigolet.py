13ac352b32607688106c34683bd3f101527b0b56
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2018-01-08 22:31:08 +0000

    Removed server enter message and anunciar. Changed mail and blocked spam from mathbot

--- trigolet.py
+++ trigolet.py
@@ -9,31 +9,40 @@ client = discord.Client()
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
-	await client.send_message(357975075468607491, content="Estudassem!")
 
 
 @client.event
 async def on_message(message):
 
 	if message.author == client.user:
+		#avoid answering to own answers
 		return
 
 	if message.content.startswith('$$$'):
 		code = message.content.split(" ")
 		command = code[0]
 		command = command[3:]
+		#excludes the $$$ from command
 		if command == "email":
-			await client.send_message(message.channel, content="Usem o suporte de problemas, idiotas!")
+			await client.send_message(message.channel, content="Usem o suporte \
+			de problemas, idiotas!")
 		elif command == "anunciar":
-			for i in range(5):
-				await client.send_message(message.channel, content= ' '.join(code[1:]))
-				
-	if 'pestana' in message.content.lower():
-		await client.send_message(message.channel, content="Onde é que anda o Gabi?")
+				await client.send_message(message.channel, content= 'Vai po \
+				caralho <@%s>' % message.author.id)
+
+	elif 'pestana' in message.content.lower():
+		await client.send_message(message.channel, \
+		content="Onde é que anda o Gabi?")
 
 	elif 'mindmap' in message.content.lower():
-		await client.send_message(message.channel, content="NOPAI")
+		await client.send_message(message.channel, \
+		content="NOPAI")
+	elif 'email' in message.content.lower():
+		await client.send_message(message.channel, content='Por não usares o \
+		suporte de problemas é que chumbaste, <@%s>' % message.author.id)
 
+	if 'http://www.wolframalpha.com/pro/' in message.content:
+		await client.delete_message(message)
 
 
 client.run('MzY2Mjg2NDY4NjUxNjc5NzY1.DOZWwg.YnR8LoBL2_LGhHCIE_ydPgN67EA')
