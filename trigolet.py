5722c353f81d0664444a21eb041f48ae10e2f5c9
Marcelo Santos <marcelocmsantos@gmail.com>
2018-01-09 14:29:51 +0000

    Add rss command

--- trigolet.py
+++ trigolet.py
@@ -3,13 +3,20 @@ from threading import Thread
 import discord
 from datetime import datetime
 import asyncio
+import time
+import re
+import json
+import feedparser
+
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
+
+def strip_html(s):
+    return re.sub('<[^<]+?>|\\xa0|&#34;', '', s)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -17,6 +24,35 @@ async def on_message(message):
     if message.author == client.user:
         #avoid answering to own answers
         return
+
+    if message.content.startswith("$$$rss"):
+        f = open('feeds.json')
+        feeds = json.load(f)
+        f.close()
+
+        new_feeds = []          # New json data to dump after fetching
+        msg = ""
+
+        for feed in feeds:
+            print(feed['name'])
+            data = feedparser.parse(feed['link'])
+            for entry in data['entries']:
+                if feed['time'] == None or \
+                   list(entry['published_parsed']) > feed['time']:
+                    msg += feed['name'] + " " + entry['link'] + "\n" + \
+                           strip_html(entry['summary']) + "\n"
+                else:
+                    break
+
+            feed['time'] = data['entries'][0]['published_parsed'] # Most recent date
+            new_feeds.append(feed)
+
+        f = open('feeds.json', 'w')
+        json.dump(new_feeds, f)
+        f.close()
+
+        await client.send_message(message.channel,
+                                  content=msg)
 
     if message.content.startswith('$$$'):
         code = message.content.split(" ")
