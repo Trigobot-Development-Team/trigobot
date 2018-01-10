74a297b50149b0e82c58f6325e9ea95901240c8d
André Breda <addobandre99@gmail.com>
2018-01-10 02:37:25 +0000

    Log command execution and errors

--- hooks/commands/__init__.py
+++ hooks/commands/__init__.py
@@ -3,6 +3,8 @@ __all__ = ['email', 'anunciar', 'rss']
 # Bring all commands into this module's scope
 from . import *
 
+import logging
+
 # TODO: add cooldown (staff unaffected)
 async def run(client, message):
     if message.content.startswith('$$$'):
@@ -10,10 +12,12 @@ async def run(client, message):
         command = parts[0]
 
         if command in __all__:
+            logging.info('{} called $$${}'.format(message.author.display_name, command))
             try:
                 await eval(command).run(client, message, args=parts[1:])
-            except:
+            except Exception as err:
                 await client.send_message(message.channel, content='Erro ao executar comando')
+                raise err
         else:
             # TODO: be funny
             await client.send_message(message.channel, content='Comando inválido')
