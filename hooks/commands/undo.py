6a3cf10b4a6950b25e2af52cf4d8affca5dcd3df
André Breda <addobandre99@gmail.com>
2018-01-11 11:36:43 +0000

    Restrict access to $$$undo: staff (relax on #botrequests and PMs)

--- hooks/commands/undo.py
+++ hooks/commands/undo.py
@@ -1,6 +1,8 @@
 from discord import Client, Message, Forbidden
+from policy import AccessControl
 import logging
 
+@AccessControl(roles=['Staff'], relax_in=['botrequests'], relax_pm=True)
 async def run(client: Client, message: Message, **kwargs):
     async for msg in client.logs_from(message.channel, limit=100):
         if msg.author == client.user:
