87bdc804b42a56085fb2c5e4d8b347f6ae0ed618
André Breda <addobandre99@gmail.com>
2018-01-11 14:28:22 +0000

    policy/access_control: Expose check_permissions for flexibility

--- policy/__init__.py
+++ policy/__init__.py
@@ -1,3 +1,3 @@
-from .access_control import AccessControl
+from .access_control import AccessControl, check_permissions
 from .cooldown import CooldownFunction, CooldownUser
 
--- policy/access_control.py
+++ policy/access_control.py
@@ -1,24 +1,22 @@
 from discord import Member, Channel
 
-import logging
-
-def AccessControl(**kwargs):
+def check_permissions(channel: Channel, user: Member, **kwargs) -> bool:
     role_whitelist = kwargs['roles']
     relaxed_channels = kwargs.get('relax_in', [])
     relax_pm = kwargs.get('relax_pm', False)
 
-    def _check_permission(channel: Channel, user: Member) -> bool:
-        if channel.name in relaxed_channels or \
-           channel.is_private:
-            return True
-        else:
-            return user.top_role in role_whitelist
+    if channel.name in relaxed_channels or \
+       (channel.is_private and user in channel.recipients and relax_pm):
+        return True
+    else:
+        return (not channel.is_private) and user.top_role in role_whitelist
 
+def AccessControl(**rules):
     def _decorate(fn):
         def _wrapper(*args, **kwargs):
             message = args[1]
 
-            if _check_permission(message.channel, message.author):
+            if check_permissions(message.channel, message.author, **rules):
                 return fn(*args, **kwargs)
             else:
                 raise PermissionError('Permission denied in {} to {}'.format(fn, message.author))
