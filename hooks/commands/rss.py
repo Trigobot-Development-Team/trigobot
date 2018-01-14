79e5fe13eac9afb7ca11584f680ac01f8d21a092
André Breda <addobandre99@gmail.com>
2018-01-14 00:42:08 +0000

    commands/rss: Fix crash when message text is too long
    Thank you IEI for helping us find this

--- hooks/commands/rss.py
+++ hooks/commands/rss.py
@@ -5,7 +5,7 @@ import logging
 
 from discord import Client, Message
 
-ANNOUNCE_CHANNEL_ID='357975075468607491'
+ANNOUNCE_CHANNEL_ID = '357975075468607491'
 
 def strip_html(s: str) -> str:
     return re.sub('<[^<]+?>|\\xa0|&#34;', '', s)
@@ -13,8 +13,14 @@ def strip_html(s: str) -> str:
 def format_feed_entry(feed_name: str, entry: dict) -> str:
     # TODO: convert HTML to Markdown for readability
     # TODO: shorten link(s) (?)
-    return '{} {} \n{}\n'.format(feed_name, entry['link'], \
-                                 strip_html(entry['summary']))
+    msg = '{} {} \n{}\n'.format(feed_name, entry['link'], \
+                                strip_html(entry['summary']))
+
+    # Shorten text when needed or Discord will refuse to send the message
+    if len(msg) > 2000:
+        msg = msg[:1994] + ' (...)'
+
+    return msg
 
 async def run(client: Client, message: Message = None, **kwargs):
     # TODO: Use a proper DB (Redis?) for optimized I/O
@@ -22,19 +28,20 @@ async def run(client: Client, message: Message = None, **kwargs):
     feeds = json.load(f)
     f.close()
 
+    channel = client.get_channel(ANNOUNCE_CHANNEL_ID)
     new_feeds = []          # New json data to dump after fetching
 
     for feed in feeds:
         data = feedparser.parse(feed['link'])
 
         for entry in data['entries']:
-            if feed['time'] == None or \
+            if feed['time'] is None or \
                 list(entry['published_parsed']) > feed['time']:
                 # TODO: consider calling client.send_message without await
                 #       instead of constructing a big message in memory
                 msg = format_feed_entry(feed['name'], entry)
 
-                await client.send_message(client.get_channel(ANNOUNCE_CHANNEL_ID), content=msg)
+                await client.send_message(channel, content=msg)
 
         feed['time'] = max(map(lambda entry: entry['published_parsed'], \
                                data['entries'])) # Most recent date
