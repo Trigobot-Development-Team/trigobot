acae0bdffc25d068ae08f4578b477901b8a0e947
André Breda <addobandre99@gmail.com>
2018-01-14 00:57:50 +0000

    hooks/commands: Split command running and exception handling logic
    Also not hacking in all the modules in the commands folder. Sane
    importlib imports only from now on.

--- hooks/commands/__init__.py
+++ hooks/commands/__init__.py
@@ -1,33 +1,39 @@
 __all__ = ['email', 'anunciar', 'rss', 'undo', 'say', 'help']
 
-# Bring all commands into this module's scope
-from . import *
-
 from discord import Client, Message
+from importlib import import_module
 import logging
+
+async def run_command(client: Client, message: Message, **kwargs):
+    parts = message.content[3:].split(' ')
+    command = parts[0]
+
+    kwargs['args'] = parts[1:]
+
+    if command in __all__:
+        logging.info('%s called $$$%s', message.author.display_name, command)
+
+        await import_module('.' + command, 'hooks.commands').run(client, message, **kwargs)
+
+    else:
+        raise NotImplementedError(command)
 
 # TODO: add cooldown (staff unaffected)
 async def run(client: Client, message: Message) -> bool:
     if message.content.startswith('$$$'):
-        parts = message.content[3:].split(" ")
-        command = parts[0]
+        try:
+            await run_command(client, message)
+        except PermissionError as err:
+            await client.send_message(message.channel, content='Accesso negado')
+            logging.info(err)
+        except NotImplementedError as err:
+            await client.send_message(message.channel, content='Comando inválido: '+str(err))
+        except Exception as err:
+            await client.send_message(message.channel, content='Erro ao executar comando')
 
-        if command in __all__:
-            logging.info('{} called $$${}'.format(message.author.display_name, command))
-            try:
-                await eval(command).run(client, message, args=parts[1:])
-            except PermissionError as err:
-                await client.send_message(message.channel, content='Accesso negado')
-                logging.info(err)
-            except Exception as err:
-                await client.send_message(message.channel, content='Erro ao executar comando')
-
-                # Raise the exception, let logging take care of it
-                # The bot won't crash, just the handler for this message
-                raise err
-        else:
-            # TODO: be funny
-            await client.send_message(message.channel, content='Comando inválido')
+            # Raise the exception, let logging take care of it
+            # The bot won't crash, just the handler for this message
+            raise err
 
         return True
     else:
