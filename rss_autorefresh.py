ed059e5fbfb1d5d74ea07a3d8f4950c9f66e7c0e
André Breda <addobandre99@gmail.com>
2018-02-11 23:28:14 +0000

    rss_autorefresh: Set interval to 15min

--- rss_autorefresh.py
+++ rss_autorefresh.py
@@ -2,7 +2,7 @@ from asyncio import sleep
 from hooks.commands.rss import run as refresh_rss
 import logging
 
-async def run(client, interval = 3600):
+async def run(client, interval = 900):
     while True:
         logging.info('Refreshing RSS feed(s)')
 
