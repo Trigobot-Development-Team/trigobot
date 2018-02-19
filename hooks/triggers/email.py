b9464ba7c86c2a69a5b18ee8d90eb86a4952e726
André Breda <addobandre99@gmail.com>
2018-02-19 15:51:38 +0000

    triggers/email: Loosen trigger conditions
    Include mail, e-mail and other *mail* triggers

--- hooks/triggers/email.py
+++ hooks/triggers/email.py
@@ -2,7 +2,7 @@ from discord import Client, Message
 
 # TODO: add cooldown
 async def run(client: Client, message: Message) -> bool:
-    if 'email' not in message.content.lower():
+    if 'mail' not in message.content.lower():
         return False
 
     await client.send_message(message.channel, \
