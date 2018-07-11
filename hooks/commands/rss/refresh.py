a1aa9043aca921211638c61e97aac0a98c024e77
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2018-07-11 16:12:07 +0100

    Add RSS update functionality
    On refresh check for differences in content for the last 5 RSS updates
    if there are it updates the Discord message with the new content
    When removing an RSS feed it deletes the hash used to check for updates

--- hooks/commands/rss/refresh.py
+++ hooks/commands/rss/refresh.py
@@ -7,8 +7,10 @@ import redis_conn
 from calendar import timegm
 from aioredis import RedisConnection
 from discord import Client, Message, Channel
+from hashlib import md5
 
 ANNOUNCE_CHANNEL_ID = '357975075468607491'
+MAX_UPDATES = 5
 
 SHORT_HELP_TEXT = str.join('\n', [
     '$$$rss - Atualiza feeds',
@@ -33,6 +35,10 @@ def format_feed_entry(feed_name: str, entry: dict) -> str:
 
     return msg
 
+def hashtext(text: str) -> int:
+    hash_object = md5(text.encode())    # generates the md5 hash of a given str
+    return hash_object.hexdigest()
+
 async def refresh_feed(client: Client, channel: Channel, redis: RedisConnection, url: str):
     logging.info('Refreshing feed %s', url)
 
@@ -48,12 +54,64 @@ async def refresh_feed(client: Client, channel: Channel, redis: RedisConnection,
             entry_timestamp > last_update:
             msg = format_feed_entry(metadata['name'], entry)
 
-            await client.send_message(channel, content=msg)
+            message = await client.send_message(channel, content=msg)
 
             if entry_timestamp > new_last_update:
                 new_last_update = entry_timestamp
 
+            # Add update to hash, field=url, value=hash+message_id
+            field = entry.link
+            value = hashtext(msg) + message.id
+            await redis.hset('updates:'+url, field, value)
+
     await redis.hset('feed:'+url, 'last_update', new_last_update)
+    await check_changes(client, channel, redis, url)
+    await keep_short(client, channel, redis, url)
+
+async def check_changes(client: Client, channel: Channel, redis: RedisConnection, url: str):
+    logging.info('Checking for changes in feed %s', url)
+
+    metadata = await redis.hgetall('feed:'+url)
+    data = feedparser.parse(url)
+    for entry in data['entries']:
+        field = entry.link
+
+        if await redis.hexists('updates:'+url, field):
+            msg = format_feed_entry(metadata['name'], entry)
+            new_hash = hashtext(msg)
+
+            value = await redis.hget('updates:'+url, field)
+            old_hash = value[:32]
+
+            if new_hash != old_hash:
+                message_id = value[32:]
+                message = await client.edit_message(message_id, content = msg)
+                value = new_hash + message.id
+                await redis.hset('updates:'+url, field, value)
+
+async def keep_short(client: Client, channel: Channel, redis: RedisConnection, url: str):
+    # If the hash has more than MAX_UPDATES updates delete the oldest one
+    hash_len = await redis.hlen('updates:'+url)
+    if hash_len > MAX_UPDATES:
+        hash_ = await redis.hgetall('updates':+url) # Returns the hash as a dict
+        hash_fields = keys(hash_)
+
+        field = hash_fields[0]
+        value = hash_[field]
+        msg_id = value[32:]
+        msg = await client.get_message(channel, msg_id)
+        early_time = msg.timestamp
+        for f in hash_fields[1:]:
+
+            temp_value = hash_[f]
+            temp_msg_id = temp_value[32:]
+            temp_msg = await client.get_message(channel, msg_id)
+
+            if temp_msg.timestamp < early_time:
+                field = temp_field
+                early_time = temp_msg.timestamp
+
+        await redis.hdel('updates:'+url, field)
 
 async def run(client: Client, message: Message = None, **kwargs):
     redis = await redis_conn.get_connection()
